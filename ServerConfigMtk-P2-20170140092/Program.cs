6dc12cae3c6ab6323166469d4680e4cce7060657
lenovo <lenovo@DESKTOP-J1DNFS8>
2021-11-22 20:33:26 +0700

    modul 6

--- ServerConfigMtk-P2-20170140092/Program.cs
+++ ServerConfigMtk-P2-20170140092/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
-using ServiceMtk_P1_20170140092;
+using ServiceMtk_P1_20170140092;
+
 
 namespace ServerConfigMtk_P2_20170140092
 {
